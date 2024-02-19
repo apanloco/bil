86b4f2da6d644471a0e9ee17dfee0a89eb32f79b
Daniel Åkerud <daniel.akerud@fossid.com>
2024-02-19 16:06:32 +0100

    Move to render.com

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 rustup target add wasm32-unknown-unknown
-cargo install basic-http-server
 cargo build --target wasm32-unknown-unknown --release
 ls -al target/wasm32-unknown-unknown/release/bil.wasm
-wasm-strip target/wasm32-unknown-unknown/release/bil.wasm
+# TODO: optimize / strip wasm
 ls -al target/wasm32-unknown-unknown/release/bil.wasm
-ln -sf target/wasm32-unknown-unknown/release/bil.wasm
-basic-http-server .
+rm -rf build
+mkdir build
+cp target/wasm32-unknown-unknown/release/bil.wasm build/
+cp -R assets build/
+cp web/* build/
